e74c286bd7dac5bfbddd7569fd9a9c5c61b8fd03
Alish Pawn <alishpau@gmail.com>
2024-05-14 11:46:58 +0545

    new-additional-pages

---
+++ 404page.php
@@ -0,0 +1,35 @@
+<!------------------------------  Header section  ------------------------------>
+<?php include("header.php"); ?>
+<!------------------------------ End  header section  ------------------------------>
+
+<!-- common-banner -->
+
+<section class="common-banner fourth-bg">
+    <div class="container cust-container">
+        <div class="background-image">
+            <img src="image/common-banner.png" alt="">
+        </div>
+        <div class="content">
+            <div class="main-title">
+                <h1 class="title f-s-2">Sorry, this page isn’t available</h1>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- common-banner -->
+
+<section class="error-page-content section-lg-gaps">
+    <div class="container cust-container">
+        <div class="content">
+            <h2 class="title f-s-4">It looks like the page you're looking for isn't here. <br>It might have been moved or taken down.</h2>
+            <div class="btn-wrap">
+                <a href="index.php" class="btn-box big">Back to home page</a>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!------------------------------  Footer section  ------------------------------>
+<?php include("footer.php"); ?>
+<!------------------------------ End  Footer section  ------------------------------>
---
+++ top-level-industry.php
@@ -0,0 +1,149 @@
+<!------------------------------  Header section  ------------------------------>
+<?php include("header.php"); ?>
+<!------------------------------ End  header section  ------------------------------>
+
+<!-- common-banner -->
+
+<section class="top-level-banner common-banner fourth-bg">
+    <div class="container">
+        <div class="background-image">
+            <img src="image/request-banner.png" alt="">
+        </div>
+        <div class="content">
+            <div class="main-title">
+                <h1 class="title f-s-2">Industry Solutions</h1>
+                <p>We have extensive industry specific experience providing cutting edge protection with our next gen cyber software and services.</p>
+            </div>
+        </div>
+    </div>
+
+    <!-- industrysolutions -->
+
+    <div class="home-industry-sec position-relative section-lg-gaps">
+        <div class="container">
+            <div class="row gy-5">
+                <div class="col-lg-5">
+                    <div class="main-title">
+                        <p>Quam erat nec elit diam massa non amet neque viverra. Sapien amet eget sodales ultrices elementum semitae eleifend pretium massa.</p>
+                    </div>
+                </div>
+                <div class="col-lg-7">
+                    <div class="row custom-gap">
+                        <div class="col-md-6">
+                            <div class="industry-box">
+                                <div class="image">
+                                    <img src="image/government.jpg" alt="">
+                                </div>
+                                <div class="content">
+                                    <h3 class="title f-s-3">Government</h3>
+                                </div>
+                                <div class="arrow-btn">
+                                    <a href="#">
+                                        <svg width="22" height="20" viewBox="0 0 22 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <g clip-path="url(#clip0_7_113)">
+                                            <path d="M21.5 10.3931H0.5" stroke="#1E1E1E" stroke-miterlimit="10" stroke-linecap="round"/>
+                                            <path d="M12.5 1.39307L21.5 10.3931" stroke="#1E1E1E" stroke-miterlimit="10" stroke-linecap="round"/>
+                                            <path d="M21.5 10.3931L12.5 19.3931" stroke="#1E1E1E" stroke-miterlimit="10" stroke-linecap="round"/>
+                                            </g>
+                                            <defs>
+                                            <clipPath id="clip0_7_113">
+                                            <rect width="22" height="19" fill="white" transform="translate(0 0.893066)"/>
+                                            </clipPath>
+                                            </defs>
+                                        </svg>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="industry-box">
+                                <div class="image">
+                                    <img src="image/critical.jpg" alt="">
+                                </div>
+                                <div class="content">
+                                    <h3 class="title f-s-3">Critical infrastructure</h3>
+                                </div>
+                                <div class="arrow-btn">
+                                    <a href="#">
+                                        <svg width="22" height="20" viewBox="0 0 22 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <g clip-path="url(#clip0_7_113)">
+                                            <path d="M21.5 10.3931H0.5" stroke="#1E1E1E" stroke-miterlimit="10" stroke-linecap="round"/>
+                                            <path d="M12.5 1.39307L21.5 10.3931" stroke="#1E1E1E" stroke-miterlimit="10" stroke-linecap="round"/>
+                                            <path d="M21.5 10.3931L12.5 19.3931" stroke="#1E1E1E" stroke-miterlimit="10" stroke-linecap="round"/>
+                                            </g>
+                                            <defs>
+                                            <clipPath id="clip0_7_113">
+                                            <rect width="22" height="19" fill="white" transform="translate(0 0.893066)"/>
+                                            </clipPath>
+                                            </defs>
+                                        </svg>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="industry-box">
+                                <div class="image">
+                                    <img src="image/business.jpg" alt="">
+                                </div>
+                                <div class="content">
+                                    <h3 class="title f-s-3">SME Business</h3>
+                                </div>
+                                <div class="arrow-btn">
+                                    <a href="#">
+                                        <svg width="22" height="20" viewBox="0 0 22 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <g clip-path="url(#clip0_7_113)">
+                                            <path d="M21.5 10.3931H0.5" stroke="#1E1E1E" stroke-miterlimit="10" stroke-linecap="round"/>
+                                            <path d="M12.5 1.39307L21.5 10.3931" stroke="#1E1E1E" stroke-miterlimit="10" stroke-linecap="round"/>
+                                            <path d="M21.5 10.3931L12.5 19.3931" stroke="#1E1E1E" stroke-miterlimit="10" stroke-linecap="round"/>
+                                            </g>
+                                            <defs>
+                                            <clipPath id="clip0_7_113">
+                                            <rect width="22" height="19" fill="white" transform="translate(0 0.893066)"/>
+                                            </clipPath>
+                                            </defs>
+                                        </svg>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="industry-box">
+                                <div class="image">
+                                    <img src="image/eduction.jpg" alt="">
+                                </div>
+                                <div class="content">
+                                    <h3 class="title f-s-3">Education</h3>
+                                </div>
+                                <div class="arrow-btn">
+                                    <a href="#">
+                                        <svg width="22" height="20" viewBox="0 0 22 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <g clip-path="url(#clip0_7_113)">
+                                            <path d="M21.5 10.3931H0.5" stroke="#1E1E1E" stroke-miterlimit="10" stroke-linecap="round"/>
+                                            <path d="M12.5 1.39307L21.5 10.3931" stroke="#1E1E1E" stroke-miterlimit="10" stroke-linecap="round"/>
+                                            <path d="M21.5 10.3931L12.5 19.3931" stroke="#1E1E1E" stroke-miterlimit="10" stroke-linecap="round"/>
+                                            </g>
+                                            <defs>
+                                            <clipPath id="clip0_7_113">
+                                            <rect width="22" height="19" fill="white" transform="translate(0 0.893066)"/>
+                                            </clipPath>
+                                            </defs>
+                                        </svg>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- industrysolutions -->
+</section>
+
+<!-- common-banner -->
+
+<!------------------------------  Footer section  ------------------------------>
+<?php include("footer.php"); ?>
+<!------------------------------ End  Footer section  ------------------------------>
